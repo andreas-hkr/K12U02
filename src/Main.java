c6501b384d9b607f574ac79311f3ef5b6ee083cd
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 17:15:27 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skriv en metod som kontrollerar om en 2D-array är symmetrisk.
+        //  - Lika många rader som kolumner
+        //  - För alla i och j gäller att [i][j] == [j][i]
+        //
+        // Testa genom att läsa in en 4x4 array i main och kalla på metoden
+
+
+    }
+}
